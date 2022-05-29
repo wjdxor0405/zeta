919014ebef55b956a6fc60b73e1e1aaeaa039ac1
wjdxor0405 <100699283+wjdxor0405@users.noreply.github.com>
2022-05-29 21:00:41 +0900

    Add files via upload

---
+++ rerwrw.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int main()
+{
+
+    double x = 0.461630;
+    double r = 0;
+
+    for (double k = 1.0 ;k < 100000000 ; k = k + 1.0)
+     {
+         r = r + 1/k - 1/(k + x);
+     }
+
+
+    cout << r <<endl;
+
+  return 0;
+}
